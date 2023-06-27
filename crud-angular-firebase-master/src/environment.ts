e87c2119461b8607bce1a567aa9c748adb254605
gabriel282828 <ga04789135373@gmail.com>
2023-06-27 19:41:56 -0300

    o

---
+++ crud-angular-firebase-master/src/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDkkDO5lN7_74pnG88l7ARFiwxBqds4CMY",
+    authDomain: "crud-angular-firebase-937d5.firebaseapp.com",
+    databaseURL: "https://crud-angular-firebase-937d5-default-rtdb.firebaseio.com",
+    projectId: "crud-angular-firebase-937d5",
+    storageBucket: "crud-angular-firebase-937d5.appspot.com",
+    messagingSenderId: "754763626703",
+    appId: "1:754763626703:web:08a9c6a99dca6ce29e9fba"
+  }
+};
